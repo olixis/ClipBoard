d69be80737f34f4dd6783c5bb8965ab4255014a3
caio fontes <olixis@gmail.com>
2016-12-04 19:55:09 -0300

    about, tos update, log system, minified js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var compression = require('compression')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,7 +8,13 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var fs = require('fs');
 var needle = require('needle');
+var emptyDir = require('empty-dir');
+var log4node = require('log4node');
+    log = new log4node.Log4Node({level: 'info', file: 'clipboard.log'});
+var favicon = require('serve-favicon');
+ 
 
+setInterval(clearTrashFolders, 1000 * 1);
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -36,6 +43,9 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+log.info(app.get('env'));
+app.disable('x-powered-by');
+app.use(favicon(__dirname + '/public/favicon/favicon.ico'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,6 +58,18 @@ app.get('/', function(req, res, next) {
     res.render('index.jade');
 });
 
+app.get('/500', function(req, res, next) {
+    res.render('500.jade');
+});
+
+app.get('/about', function(req, res, next) {
+    res.render('about.jade');
+});
+
+app.get('/404', function(req, res, next) {
+    res.render('404.jade');
+});
+
 app.get('/tos', function(req, res, next) {
     res.render('tos.jade');
 });
@@ -56,66 +78,56 @@ app.get('/tos', function(req, res, next) {
 app.post('/', upload.single('file'), function(req, res, next) {
     var path = req.file.path;
     var newPath = path.substring(0, 14) + req.file.originalname;
-    console.log(req.ip + "uploaded: " + req.file.originalname);
-    fs.renameSync(path, newPath);
-    //deleteAfterUpload(newPath);
-    var downloadPath = newPath.substring(8, 13);
-    var url = 'http://clipboard.host/download/' + downloadPath + '/' + req.file.originalname;
+    log.info(req.ip + "uploaded: " + req.file.originalname);
+    fs.rename(path, newPath, function(err) {
+        var downloadPath = newPath.substring(8, 13);
+        var url = 'http://clipboard.host/download/' + downloadPath + '/' + req.file.originalname;
 
-    needle.secrepath = newPath;
-    needle.post('https://www.google.com/recaptcha/api/siteverify', {
-        secret: '6LcZtQ0UAAAAAPr5MbPk2RTk1h2yVT2e8vtJfReU',
-        response: req.body['g-recaptcha-response']
-    }, function(err, response, body) {
-        if (err) {
-            deleteAfterUpload(__dirname + '/' + newPath, __dirname + '/' + dir);
-            res.end();
-        } else {
-            if (body.success) {
-                res.send('The download link for your file is: ' + url);
+        needle.secrepath = newPath;
+        needle.post('https://www.google.com/recaptcha/api/siteverify', {
+            secret: '6LcZtQ0UAAAAAPr5MbPk2RTk1h2yVT2e8vtJfReU',
+            response: req.body['g-recaptcha-response']
+        }, function(err, response, body) {
+            if (err) {
+                deleteAfterUpload(__dirname + '/' + newPath, __dirname + '/' + dir, next, res);
+                //next(Error('Not Found'));
             } else {
-                var dir = needle.secrepath.split('/')[0] + '/' + needle.secrepath.split('/')[1];
-                deleteAfterUpload(__dirname + '/' + newPath, __dirname + '/' + dir);
-                res.render('error.jade', {
-                    error: 'olar'
-                })
+                if (body.success) {
+                    res.send('The download link for your file is: ' + url);
+                } else {
+                    var dir = needle.secrepath.split('/')[0] + '/' + needle.secrepath.split('/')[1];
+                    deleteAfterUpload(__dirname + '/' + newPath, __dirname + '/' + dir, next, res);
+                    //next(Error('Not Found'));
+                }
             }
-        }
+        });
     });
-
-
-
-
-
-    //res.send('The download link for your file is: ' + link);
-    //res.render('index', {title: 'Express'});
-
 });
 
 app.get('/download/:folder/:filename', function(req, res, next) {
     var filename = req.params.filename;
     var folder = req.params.folder;
-    console.log(req.ip + "downloaded: " + folder + '\\' + filename);
+    log.info(req.ip + "downloaded: " + folder + '\\' + filename);
     res.download(__dirname + '/uploads' + '/' + folder + '/' + filename, filename, function(err) {
         if (err) {
-            console.log(err);
-            res.end();
+            log.error(err);
+            res.render('404.jade');
         } else {
             var pathFile = __dirname + '/uploads' + '/' + folder + '/' + filename;
             var pathDir = __dirname + '/uploads' + '/' + folder + '/';
-            deleteAfterUpload(pathFile, pathDir)
+            deleteAfterUpload(pathFile, pathDir, next, res)
         }
     });
 });
 
-var deleteAfterUpload = function(path, pathDir) {
-
+var deleteAfterUpload = function(path, pathDir, next, res) {
     fs.unlink(path, function(err) {
-        if (err) console.log(err);
-        fs.rmdirSync(pathDir);
-        console.log('file ' + path + ' successfully deleted');
+        if (err) log.error(err);
+        fs.rmdir(pathDir, function(err) {
+            next('Finish the captcha Properly.');
+        });
+        log.info('file ' + path + ' successfully deleted');
     });
-
 };
 
 function errorHandler(err, req, res, next) {
@@ -128,6 +140,7 @@ function errorHandler(err, req, res, next) {
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(errorHandler);
@@ -138,7 +151,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 //catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -148,30 +161,31 @@ app.use(function(req, res, next) {
         error: err
     })
 });
-//
-//// error handlers
-//
-//// development error handler
-//// will print stacktrace
-//if (app.get('env') === 'development') {
-//    app.use(function (err, req, res, next) {
-//        res.status(err.status || 500);
-//        res.render('error', {
-//            message: err.message,
-//            error: err
-//        });
-//    });
-//}
-//
-//// production error handler
-//// no stacktraces leaked to user
-//app.use(function (err, req, res, next) {
-//    res.status(err.status || 500);
-//    res.render('error', {
-//        message: err.message,
-//        error: {}
-//    });
-//});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 function makeid() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -180,6 +194,26 @@ function makeid() {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
+}
+
+function clearTrashFolders() {
+    fs.readdir(__dirname + '/' + 'uploads', function(err, listaDir) {
+        for (var index = 0; index < listaDir.length; index++) {
+            var element = listaDir[index];
+            var caminhoVazios = __dirname + '/' + 'uploads' + '/' + element;
+            emptyDir(caminhoVazios, function(err, result) {
+                if (result) {
+                    fs.rmdir(caminhoVazios, function(err) {
+                        if (err) {
+                        } else {
+                           log.info('diretorio lixo: ' + caminhoVazios + ' foi apagado com sucesso!');
+                        }
+                    });
+                }
+            });
+        }
+    });
+
 }
 
 module.exports = app;
